f639be567c4150484ec28932178ad35ea557f30c
Jitesh-21 <jitesh.adesara218@gmail.com>
2019-02-11 13:53:42 +0530

    first git commit for my demo code

---
+++ src/test/java/DemoGit/DemoGit/AppTest.java
@@ -0,0 +1,22 @@
+package DemoGit.DemoGit;
+
+
+ 
+public class AppTest 
+{
+	
+public static void main ( String args[])
+{
+System.out.println("This is Hello world Program");	
+}
+
+
+
+
+
+}
+
+
+
+
+
