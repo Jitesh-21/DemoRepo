bf43ba4a770d4d385e8cdcc3edecd3a67d23ca9e
Jitesh-21 <jitesh.adesara218@gmail.com>
2019-02-12 12:34:24 +0530

    my second commit for learning purpose

--- src/test/java/DemoGit/DemoGit/AppTest.java
+++ src/test/java/DemoGit/DemoGit/AppTest.java
@@ -8,6 +8,9 @@ public class AppTest
 public static void main ( String args[])
 {
 System.out.println("This is Hello world Program");	
+System.out.println("This is for new change in program");	
+System.out.println("This is nice ");	
+System.out.println("This is good");	
 }
 
 
---
+++ src/test/java/DemoGit/DemoGit/Test1.java
@@ -0,0 +1,10 @@
+package DemoGit.DemoGit;
+
+public class Test1 {
+	
+	public static void main (String [] args)
+	{
+		
+		System.out.println("This is my new class and program");
+	}
+}
